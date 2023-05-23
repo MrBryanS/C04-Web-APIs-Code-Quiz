e6ee85af4fbe894c01f09345b84b5a960fc30c99
Bryan Stauning <bryanstauning@gmail.com>
2023-05-23 05:28:44 -0500

    Add html pages and start gamerules.js

---
+++ js/gamerules.js
@@ -0,0 +1,23 @@
+var questions = [
+    {Question: "Which is not a JS Data Type?"
+    Choices: ['Pickles', 'Boolean', 'String', 'Integer'],
+    answer: 'Pickles'
+    }
+];
+
+var questions = [
+    {Question: "Which is not a JS Data Type?"
+    Choices ['Pickles', 'Boolean', 'String', 'Integer'],
+    answer: 'Pickles'
+    }
+];
+
+var questions = [
+    {Question: "Which is not a JS Data Type?"
+    Choices ['Pickles', 'Boolean', 'String', 'Integer'],
+    answer: 'Pickles'
+    }
+];
+questions[currentQuestion].question
+questions[currentQuestion].choices
+questions[currentQuestion].answer
